206a4e933a226e1d7666a0e898476f03e4b905d9
pradeep kumar patel <88222189+itspradeep98@users.noreply.github.com>
2024-01-08 23:45:24 +0530

    nav-list -> navlist

--- web_1/js/resp.js
+++ web_1/js/resp.js
@@ -6,7 +6,7 @@ rightnav =  document.querySelector('.rightnav')
 
 burger.addEventListener('click', ()=>{
     navbar.classList.toggle('v-class-resp');
-    nav-list.classList.toggle('v-class-resp');
+    navlist.classList.toggle('v-class-resp');
     navbar.classList.toggle('h-class-resp');
 
 })
